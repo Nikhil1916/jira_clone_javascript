7365d72685baed3cf5cdaa042b2d15a89031579e
Nikhil Chawla <nikhil1916.cse18@chitkara.edu.in>
2023-07-23 17:16:40 +0530

    added filtering acc. to priotity & local stroage to save tickets

--- script.js
+++ script.js
@@ -8,14 +8,12 @@ const textArea = document.querySelector('.writing-area');
 let isModalPresent = false;
 const colors = ['lightpink', 'lightgreen', 'lightblue', 'black'];
 let modalPriorityColor = colors[colors.length - 1];
-// console.log(modal)
 const mainCont = document.querySelector('.main-cont');
-
 const allPriorityColors = document.querySelectorAll('.priority-color');
-console.log(allPriorityColors);
+const priorityGetter = document.querySelectorAll('#priorities>*');
+let ticketArr = [];
 
 addBtn.addEventListener('click', function (e) {
-  console.log(e);
   // toggle modal display
   if (isModalPresent) {
     modal.style.display = 'none'
@@ -31,7 +29,7 @@ modalWritePart.addEventListener('keydown', function (e) {
   if (e.key == 'Shift') {
     //create ticket
     //toggle display and hide model
-    createTicket(modalPriorityColor, '#' + uid(), textArea.value);
+    createTicket(modalPriorityColor, '#' + uid(), textArea.value, true);
     modal.style.display = 'none';
     isModalPresent = false;
     textArea.value = ''
@@ -39,10 +37,10 @@ modalWritePart.addEventListener('keydown', function (e) {
   }
 })
 
-function createTicket(ticketColor, id, text) {
+function createTicket(ticketColor, id, text, isNewTicket) {
   let ticketContainer = document.createElement('div');
   ticketContainer.setAttribute('class', 'ticket-cont');
-  ticketContainer.innerHTML = `
+  ticketContainer.innerHTML = ` 
   <div class='ticket-color ${ticketColor}'></div>
   <div class='ticket-id'>${id}</div>
   <div class='task-area'>${text}</div>
@@ -50,6 +48,25 @@ function createTicket(ticketColor, id, text) {
   <i class="fa-solid fa-lock"></i></div>
   `;
   mainCont.appendChild(ticketContainer);
+
+  // add ticket in local storage when ticket is being created for first time
+  if (localStorage.getItem('tickets') == null) {
+    localStorage.setItem('tickets', JSON.stringify([]));
+  }
+  // const arr = JSON.parse(localStorage.getItem('tickets'));
+  // if (arr.find((ticket) => ticket.id == id)) {
+  //   return;
+  // }
+  if (!isNewTicket) {
+    return;
+  }
+  ticketArr.push({
+    color: ticketColor,
+    id,
+    text
+  })
+  localStorage.setItem('tickets', JSON.stringify(ticketArr));
+
 }
 
 
@@ -62,3 +79,27 @@ allPriorityColors.forEach(color_el => {
     modalPriorityColor = color_el.classList[0];
   })
 })
+
+
+// getting tickets on basis of ticket color
+priorityGetter.forEach((priority_color) => {
+  priority_color.addEventListener('click', function (e) {
+    const allTickets = document.querySelectorAll(".main-cont>*");
+    allTickets.forEach((ticket) => ticket.remove());
+    const currColor = priority_color.classList[0];
+    let filteredArr = ticketArr?.filter((ticket) => ticket.color == currColor);
+    filteredArr.forEach(ticket => createTicket(ticket.color, ticket.id, ticket.text))
+  })
+  priority_color.addEventListener('dblclick', function () {
+    const allTickets = document.querySelectorAll(".main-cont>*");
+    allTickets.forEach((ticket) => ticket.remove());
+    ticketArr.forEach((ticket) => createTicket(ticket.color, ticket.id, ticket.text));
+  })
+})
+
+
+// getting all data from local storage for re rendering of tickets
+if (localStorage.getItem('tickets')) {
+  ticketArr = JSON.parse(localStorage.getItem('tickets'));
+  ticketArr.forEach((ticket) => createTicket(ticket.color, ticket.id, ticket.text));
+}
